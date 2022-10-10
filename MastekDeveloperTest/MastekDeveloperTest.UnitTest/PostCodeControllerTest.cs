17b8b16e0ef8344d7f8c5656d7d731f96130b68c
Dnyaneshwar.Shivbhakta <shivbhaktadnyaneshwar.1@gmail.com>
2022-10-10 17:13:36 +0530

    Unit test cases

---
+++ MastekDeveloperTest/MastekDeveloperTest.UnitTest/PostCodeControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using MastekDeveloperTest.Controllers;
+using MastekDeveloperTest.Service;
+using MastekTest.PostCodeRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace MastekDeveloperTest.UnitTest
+{
+    public class PostCodeControllerTest
+    {
+        #region private vairable
+        private readonly PostCodeController _postCodeController;
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Business Profile Controller Property initialization for Test
+        /// </summary>
+        public PostCodeControllerTest()
+        {
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MapperProfile());
+            });
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddEnvironmentVariables();
+            var Configuration = configurationBuilder.Build();
+            var config= new ConfigurationBuilder()
+                .AddConfiguration(Configuration)
+                .AddJsonFile($"appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+           
+           var repository = new PostCodesRepository(config);
+           _postCodeController = new PostCodeController(new PostCodeService(repository, mockMapper.CreateMapper()));
+        }
+        #endregion
+        #region Test Method
+        /// <summary>
+        /// Get zip code list based on type ahead character A
+        /// </summary>
+        [Theory]
+        [InlineData("A")]
+        public async Task Controller_should_get_auto_completion_list(string code)
+        {
+           var result = await _postCodeController.GetAutoCompletionList(code);
+            Assert.True(result?.Count > 0);
+        }
+
+        /// <summary>
+        /// Get zip code details based on type ahead character A
+        /// </summary>
+        [Theory]
+        [InlineData("BN7 1AD")]
+        public async Task Controller_should_get_area_details(string code)
+        {
+            var result = await _postCodeController.Get(code);
+            Assert.True(result?.Count > 0);
+        }
+
+        #endregion
+    }
+}
