a88d26ffc4e0fc3ec5ba84d8bf946e9a9146306d
Dnyaneshwar.Shivbhakta <shivbhaktadnyaneshwar.1@gmail.com>
2022-10-10 16:26:25 +0530

    removing extra space

--- MastekDeveloperTest/MastekTest.DTO/MastekAreaDto.cs
+++ MastekDeveloperTest/MastekTest.DTO/MastekAreaDto.cs
@@ -8,13 +8,11 @@ namespace MastekDeveloperTest.DTO
     {
         public string Country { get; set; }
         public string Region { get; set; }
-
         [JsonProperty(PropertyName = "admin_district")]
         public string AdminDistrict { get; set; }
         [JsonProperty(PropertyName = "parliamentary_constituency")]
         public string ParlimentaryConstituency { get; set; }
         public double Latitude { get; set; }
-
         public string Area { get; set; }
     }
 
